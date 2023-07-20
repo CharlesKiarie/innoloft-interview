28c2a64f367c568da2c9bacb3469353494adc4f5
crushy77 <kiariecharles77@gmail.com>
2023-07-21 02:28:31 +0300

    Added rollup config to vite test

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import commonjs from 'vite-plugin-commonjs'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
 
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), commonjs({
-    filter(id) {
-      // `node_modules` is exclude by default, so we need to include it explicitly
-      // https://github.com/vite-plugin/vite-plugin-commonjs/blob/v0.7.0/src/index.ts#L125-L127
-      if (id.includes('node_modules/xxx')) {
-        return true
-      }
-    }
-  })],
+  plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
     environment: 'happy-dom',
     setupFiles: '.vitest/setup',
     include: ['**/test.{ts,tsx}']
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        file: 'bundle.js',
+        format: 'iife',
+        name: 'MyModule'
+      },
+      plugins: [commonjs(), resolve()]
+    }
   }
 })
