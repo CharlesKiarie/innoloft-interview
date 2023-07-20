327260c1bf8d5781eacd73e683530a63ea281e39
crushy77 <kiariecharles77@gmail.com>
2023-07-21 02:17:12 +0300

    Installed vite-common js

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import resolve from '@rollup/plugin-node-resolve'
+import commonjs from 'vite-plugin-commonjs'
 
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), resolve()],
+  plugins: [react(), tsconfigPaths(), commonjs({
+    filter(id) {
+      // `node_modules` is exclude by default, so we need to include it explicitly
+      // https://github.com/vite-plugin/vite-plugin-commonjs/blob/v0.7.0/src/index.ts#L125-L127
+      if (id.includes('node_modules/xxx')) {
+        return true
+      }
+    }
+  })],
   test: {
     globals: true,
     environment: 'happy-dom',
