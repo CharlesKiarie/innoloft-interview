4cd54521cbf4b1fd8716b46914019e219c28cf97
crushy77 <kiariecharles77@gmail.com>
2023-07-21 00:05:07 +0300

    Changed browserrouter to hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import 'tailwindcss/tailwind.css'
 import App from 'features/App'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { store } from 'store/store'
 import { Provider } from 'react-redux'
 
@@ -9,9 +9,9 @@ const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
 
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <App />
         </Provider>
-    </BrowserRouter>
+    </HashRouter>
 )
